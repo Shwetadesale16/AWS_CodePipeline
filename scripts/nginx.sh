9c79a0972d02c1dc3665ffd21695a68de550abc6
root <root@ip-172-31-26-127.us-west-2.compute.internal>
2024-09-29 09:57:50 +0000

    fixed issues for permissions

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -4,10 +4,10 @@
 sudo systemctl daemon-reload
 sudo rm -f /etc/nginx/sites-enabled/default
 
-sudo cp /home/ubuntu/Project_folder_name/nginx/nginx.conf /etc/nginx/sites-available/fodler_name_where_seetngs.py_is
-sudo ln -s /etc/nginx/sites-available/fodler_name_where_seetngs.py_is /etc/nginx/sites-enabled/
+sudo cp /home/ubuntu/AWS_CodePipeline/nginx/nginx.conf /etc/nginx/sites-available/blog
+sudo ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled
 #sudo ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled
 #sudo nginx -t
-sudo gpasswd -a www-data ubuntu
+sudo gpasswd -a www-data root
 sudo systemctl restart nginx
 
